6950d234d7c758eaf42a67d6b375f0453698d2bd
shayat <shayat@innexiv.com>
2026-08-27 00:29:01 +0500

    Assignment 2 Stage 1: database read endpoints

--- server.js
+++ server.js
@@ -43,7 +43,9 @@ const tasks = [
 
 app.get("/tasks/:id", (req, res) => {
     const id = Number(req.params.id);
-    const task = tasks.find(task => task.id === id);
+    const task = db
+        .prepare("SELECT * FROM tasks WHERE id = ?")
+        .get(id);
 
     if (!task) {
         return res.status(404).json({
@@ -55,6 +57,7 @@ app.get("/tasks/:id", (req, res) => {
 });
 
 app.get("/tasks", (req, res) => {
+    const tasks = db.prepare("SELECT * FROM tasks").all();
     res.json(tasks);
 });
 
